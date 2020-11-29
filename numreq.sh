0c0e0e358c5ce14f420873adb38daf1973962865
DalboniP <pdalbonii@gmail.com>
2020-11-29 11:02:36 -0300

    Delete numreq.sh

--- numreq.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#### this script let you organize all the ip in an access.log, by number of requests
-#### ---log analysis script ---
-#### usage: numreq.sh access.log
-cat $1 | cut -d " " -f 1 | sort | uniq -c | sort -unr
